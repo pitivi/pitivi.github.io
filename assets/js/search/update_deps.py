bed53868ebcda79836bdcb1a43941b63e140d8b5
Alexandru Băluț <alexandru.balut@gmail.com>
2025-03-09 23:44:44 +0100

    Update

---
+++ assets/js/search/update_deps.py
@@ -0,0 +1,1 @@
+urls_downloaded_cb({"token":"update_deps.py","urls":[{"url":"Updating_Python_dependencies.html#python-dependencies","node_type":"p","page":"Python dependencies","sections":["Python dependencies"],"context":{"gi-language":["default"]}}]});
